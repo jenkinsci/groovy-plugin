1fe281234fec2edd01da61b25f336035d339c93a
dvrzalik <dvrzalik@dev.java.net>
2008-03-05 00:32:02 +0000

    Initial import of Groovy plugin.

---
+++ src/main/java/hudson/plugins/groovy/PluginImpl.java
@@ -0,0 +1,18 @@
+package hudson.plugins.groovy;
+
+import hudson.Plugin;
+import hudson.tasks.BuildStep;
+
+/**
+ * Entry point of Groovy plugin.
+ *
+ * @author dvrzalik
+ * @plugin
+ */
+public class PluginImpl extends Plugin {
+    public void start() throws Exception {
+       
+        BuildStep.BUILDERS.add(Groovy.DESCRIPTOR);
+        BuildStep.BUILDERS.add(SystemGroovy.DESCRIPTOR);
+    }
+}
