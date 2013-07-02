e4a2e8e5c186d6db49335e7f15cb447b4b2e24ad
Vojtech Juranek <vjuranek@redhat.com>
2013-07-02 12:03:33 +0200

    Update tests to JenkinsRule

--- src/test/java/hudson/plugins/groovy/PluginTest.java
+++ src/test/java/hudson/plugins/groovy/PluginTest.java
@@ -1,18 +1,26 @@
 package hudson.plugins.groovy;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class PluginTest extends HudsonTestCase {
+public class PluginTest {
 
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+    
+    @Test
     public void testAllowTokenMacro() throws ElementNotFoundException, Exception {
-        HtmlPage page = createWebClient().goTo("configure");
+        HtmlPage page = j.createWebClient().goTo("configure");
         HtmlCheckBoxInput allowMacroCheckBox = page.getElementByName("allowMacro");
         allowMacroCheckBox.setChecked(true);
-        submit(page.getFormByName("config"));
+        j.submit(page.getFormByName("config"));
         assertTrue(Groovy.DESCRIPTOR.getAllowMacro());
     }
 }
