1ccbfe539b3f1e3364ff4af3683aefbb056f65b7
Vojtech Juranek <vjuranek@redhat.com>
2014-01-31 00:29:34 +0100

    Add syntax validation button

--- src/main/java/hudson/plugins/groovy/StringScriptSource.java
+++ src/main/java/hudson/plugins/groovy/StringScriptSource.java
@@ -1,19 +1,22 @@
 package hudson.plugins.groovy;
 
+import groovy.lang.GroovyShell;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.model.Descriptor;
+import hudson.util.FormValidation;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.sf.json.JSONObject;
+import jenkins.model.Jenkins;
 
+import org.codehaus.groovy.control.CompilationFailedException;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
 
 /**
  * Groovy script specified by command string.
@@ -33,33 +36,32 @@ public class StringScriptSource implements ScriptSource {
     public InputStream getScriptStream(FilePath projectWorkspace) {
         return new ByteArrayInputStream(command.getBytes());
     }
-    
+
     @Override
     public InputStream getScriptStream(FilePath projectWorkspace, AbstractBuild<?, ?> build, BuildListener listener) {
         return getScriptStream(projectWorkspace);
     }
-    
+
     @Override
     public FilePath getScriptFile(FilePath projectWorkspace) throws IOException, InterruptedException {
         return projectWorkspace.createTextTempFile("hudson", ".groovy", command, true);
     }
-    
+
     @Override
-    public FilePath getScriptFile(FilePath projectWorkspace, AbstractBuild<?, ?> build, BuildListener listener) throws IOException, InterruptedException {
+    public FilePath getScriptFile(FilePath projectWorkspace, AbstractBuild<?, ?> build, BuildListener listener)
+            throws IOException, InterruptedException {
         return getScriptFile(projectWorkspace);
     }
 
     public String getCommand() {
-      return command;
+        return command;
     }
 
     public Descriptor getDescriptor() {
-        return DESCRIPTOR;
+        return (DescriptorImpl) Jenkins.getInstance().getDescriptor(getClass());
     }
 
     @Extension
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
-
     public static class DescriptorImpl extends Descriptor<ScriptSource> {
 
         public DescriptorImpl() {
@@ -71,10 +73,17 @@ public class StringScriptSource implements ScriptSource {
             return "Groovy command";
         }
 
-        @Override
-        public ScriptSource newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return req.bindJSON(StringScriptSource.class, formData);
+        public FormValidation doCheckScript(@QueryParameter String command) {
+            if (command == null || command.trim().isEmpty())
+                return FormValidation.error("Script seems to be empty string!");
+
+            try {
+                new GroovyShell().parse(command);
+                return FormValidation.ok("So far so good");
+            } catch (CompilationFailedException e) {
+                return FormValidation.error(e.getMessage());
+            }
         }
-        
+
     }
 }
