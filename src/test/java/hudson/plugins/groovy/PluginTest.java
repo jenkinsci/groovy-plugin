b0bcc20937258dddb734dc842ba3ff190c0b716f
Vojtech Juranek <vjuranek@redhat.com>
2014-11-13 23:35:36 +0100

    [JENKINS-25392] Add test which verifies that build fails when exception is thrown from script

--- src/test/java/hudson/plugins/groovy/PluginTest.java
+++ src/test/java/hudson/plugins/groovy/PluginTest.java
@@ -1,6 +1,8 @@
 package hudson.plugins.groovy;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import hudson.model.Result;
 import hudson.model.FreeStyleProject;
 import hudson.tasks.Builder;
 
@@ -24,6 +26,17 @@ public class PluginTest {
         j.submit(page.getFormByName("config"));
         Groovy.DescriptorImpl descriptor = (Groovy.DescriptorImpl) j.jenkins.getDescriptor(Groovy.class);
         assertTrue(descriptor.getAllowMacro());
+    }
+    
+    //JENKINS-25392
+    @Test
+    public void testFailWhenScriptThrowsException() throws Exception {
+        ScriptSource script = new StringScriptSource("throw new Exception(\"test\")");
+        Groovy g = new Groovy(script,"(Default)", "", "","", "", "");
+        FreeStyleProject p = j.createFreeStyleProject();
+        p.getBuildersList().add(g);
+        assertEquals(Result.FAILURE, p.scheduleBuild2(0).get().getResult());
+        
     }
 
     @Test
