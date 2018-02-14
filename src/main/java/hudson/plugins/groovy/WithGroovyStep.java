115b77fb7a2428c490acff25c3ac443ddf4cfe40
Jesse Glick <jglick@cloudbees.com>
2018-02-14 14:12:36 -0500

    Sketch of rewrite without https://github.com/jenkinsci/workflow-cps-plugin/pull/59.

---
+++ src/main/java/hudson/plugins/groovy/WithGroovyStep.java
@@ -0,0 +1,149 @@
+package hudson.plugins.groovy;
+
+import groovy.lang.GroovyObject;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Util;
+import hudson.remoting.ClassFilter;
+import hudson.remoting.ObjectInputStreamEx;
+import hudson.slaves.WorkspaceList;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.CheckForNull;
+import org.jenkinsci.plugins.workflow.steps.BodyExecutionCallback;
+import org.jenkinsci.plugins.workflow.steps.Step;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+/**
+ * Makes {@code groovy} in {@code PATH} be bound to an appropriate executable.
+ */
+public class WithGroovyStep extends Step {
+
+    private String tool;
+    private Object input;
+
+    @DataBoundConstructor
+    public WithGroovyStep() {}
+
+    public String getTool() {
+        return tool;
+    }
+
+    @DataBoundSetter
+    public void setTool(String tool) {
+        this.tool = Util.fixEmpty(tool);
+    }
+
+    public Object getInput() {
+        return input;
+    }
+
+    @DataBoundSetter
+    public void setInput(Object input) {
+        this.input = input;
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new Execution(context, this);
+    }
+
+    // TODO use 1.652 use WorkspaceList.tempDir
+    private static FilePath tempDir(FilePath ws) {
+        return ws.sibling(ws.getName() + System.getProperty(WorkspaceList.class.getName(), "@") + "tmp");
+    }
+
+    private static class Execution extends StepExecution {
+
+        private final transient WithGroovyStep step;
+
+        Execution(StepContext context, WithGroovyStep step) {
+            super(context);
+            this.step = step;
+        }
+
+        @Override
+        public boolean start() throws Exception {
+            FilePath base = tempDir(getContext().get(FilePath.class));
+            base.mkdirs();
+            FilePath tmp = base.createTempDir("groovy", "");
+            // TODO create wrapper script $tmp/groovy paying attention to tool and JDK
+            if (step.input != null) {
+                // TODO remoting calls here are going to be blocking CPS VM thread
+                try (OutputStream os = tmp.child("input.ser").write(); ObjectOutputStream oos = new ObjectOutputStream(os)) {
+                    oos.writeObject(step.input);
+                }
+                tmp.child("Pipeline.groovy").copyFrom(WithGroovyStep.class.getResource("Pipeline.groovy"));
+                // TODO define -Dpipeline.io.dir=$tmp
+            }
+            getContext().newBodyInvoker().
+                // TODO expand $PATH with tmp
+                withCallback(new Callback(tmp)).
+                start();
+            return false;
+        }
+
+    }
+
+    private static class Callback extends BodyExecutionCallback {
+
+        private final String tmp;
+
+        Callback(FilePath tmp) {
+            this.tmp = tmp.getRemote();
+        }
+
+        @Override
+        public void onSuccess(StepContext context, Object result) {
+            try {
+                FilePath ser = context.get(FilePath.class).child(tmp).child("output.ser");
+                if (ser.exists()) {
+                    try (InputStream is = ser.read(); ObjectInputStream ois = new ObjectInputStreamEx(is, GroovyObject.class.getClassLoader(), ClassFilter.DEFAULT)) {
+                        context.onSuccess(ois.readObject());
+                        return;
+                    }
+                }
+            } catch (Exception x) {
+                context.onFailure(x);
+                return;
+            }
+            context.onSuccess(result);
+        }
+
+        @Override
+        public void onFailure(StepContext context, Throwable t) {
+            context.onFailure(t);
+        }
+
+    }
+
+    @Extension
+    public static class DescriptorImpl extends StepDescriptor {
+
+        @Override
+        public String getFunctionName() {
+            return "withGroovy";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Execute Groovy script";
+        }
+
+        @Override
+        public Set<Class<?>> getRequiredContext() {
+            return Collections.<Class<?>>singleton(FilePath.class);
+        }
+
+    }
+
+}
--- src/test/java/hudson/plugins/groovy/WithGroovyStepTest.java
+++ src/test/java/hudson/plugins/groovy/WithGroovyStepTest.java
@@ -9,7 +9,7 @@ import org.junit.Rule;
 import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
 
-public class GroovyPipelineStepTest {
+public class WithGroovyStepTest {
 
     @ClassRule
     public static BuildWatcher buildWatcher = new BuildWatcher();
@@ -21,14 +21,14 @@ public class GroovyPipelineStepTest {
     public void smokes() throws Exception {
         r.createSlave("remote", null, null);
         WorkflowJob p = r.createProject(WorkflowJob.class, "p");
-        p.setDefinition(new CpsFlowDefinition("node('remote') {writeFile file: 'x.groovy', text: 'println(/got: ${111/3}/)'; groovy 'x.groovy'}", true));
+        p.setDefinition(new CpsFlowDefinition("node('remote') {writeFile file: 'x.groovy', text: 'println(/got: ${111/3}/)'; withGroovy {sh 'groovy x.groovy'}}", true));
         r.assertLogContains("got: 37", r.buildAndAssertSuccess(p));
     }
 
     @Test
     public void configRoundtrip() throws Exception {
         StepConfigTester tester = new StepConfigTester(r);
-        GroovyPipelineStep step = new GroovyPipelineStep("-Dwhatever script.groovy");
+        WithGroovyStep step = new WithGroovyStep();
         r.assertEqualDataBoundBeans(step, tester.configRoundTrip(step));
         r.jenkins.getDescriptorByType(GroovyInstallation.DescriptorImpl.class).setInstallations(new GroovyInstallation("groovy3", "/usr/share/groovy3", null));
         r.assertEqualDataBoundBeans(step, tester.configRoundTrip(step));
@@ -40,7 +40,7 @@ public class GroovyPipelineStepTest {
     public void io() throws Exception {
         WorkflowJob p = r.createProject(WorkflowJob.class, "p");
         r.jenkins.getWorkspaceFor(p).child("calc.groovy").write("Pipeline.output(Pipeline.input().collect {k, v -> k * v})", null);
-        p.setDefinition(new CpsFlowDefinition("node {def r = groovy args: 'calc.groovy', input: [once: 1, twice: 2, thrice: 3]; echo r.join('/')}", true));
+        p.setDefinition(new CpsFlowDefinition("node {def r = withGroovy(input: [once: 1, twice: 2, thrice: 3]) {sh 'groovy calc.groovy'}; echo r.join('/')}", true));
         r.assertLogContains("once/twicetwice/thricethricethrice", r.buildAndAssertSuccess(p));
     }
 
